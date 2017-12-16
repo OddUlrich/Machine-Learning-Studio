47d58cca158db9416c0f1c7659d6d74851d7a618
Ulrich <vmoonodd@gmail.com>
2017-12-16 14:56:45 +0800

    finish the tutorials

--- scikit-ext/sklearn_ml_example.py
+++ scikit-ext/sklearn_ml_example.py
@@ -9,6 +9,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 
+# Evaluate model performance.
+from sklearn.metrics import mean_squared_error, r2_score
+
 # Persist out model forr future use.
 from sklearn.externals import joblib
 
@@ -57,3 +60,39 @@ print X_test_scaled.std(axis = 0)
 # Pipeline with preprocessing and model.
 pineline = make_pipeline(preprocessing.StandardScaler(), 
                          RandomForestRegressor(n_estimators = 100))
+
+# List tunable hyperparameters
+print pineline.get_params()
+# ...
+# 'randomforestregressor__criterion': 'mse',
+# 'randomforestregressor__max_depth': None,
+# 'randomforestregressor__max_features': 'auto',
+# 'randomforestregressor__max_leaf_nodes': None,
+# ...
+
+# Declare hyperparameters to tune
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
+
+# Cross-validation
+clf = GridSearchCV(pineline, hyperparameters, cv=10)
+
+# Fit and tune model
+clf.fit(X_train, y_train)
+
+print clf.best_params_
+
+# Refit on the entire training set.(On by default)
+print clf.refit
+
+# Evaluate the model pineline on test data.
+y_pred = clf.predict(X_test)
+print r2_score(y_test, y_pred)
+print mean_squared_error(y_test, y_pred)
+
+# Save the model for future use.
+joblib.dump(clf, 'rf_regressor.pkl')
+
+# Load model from .pkl file
+clf2 = joblib.load('rf_regressor.pkl')
+clf2.predict(X_test)
