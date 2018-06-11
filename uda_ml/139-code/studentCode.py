7a402d6763fd92bc30e1b9a980a92a4b36e210d8
Ulrich <vmoonodd@gmail.com>
2018-06-11 21:27:20 +0800

    svm and linear trainer in scikit-learn

--- uda_ml/139-code/studentCode.py
+++ uda_ml/139-code/studentCode.py
@@ -7,6 +7,7 @@
 
 """
 
+from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from sklearn.svm import SVC
 
@@ -25,8 +26,10 @@ labels = iris.target
 ### set the random_state to 0 and the test_size to 0.4 so
 ### we can exactly check your result
 
-
-
+features_train, features_test, labels_train, labels_test = train_test_split(iris.data, 
+																			iris.target, 
+																			test_size=0.4, 
+																			random_state=0)
 
 ###############################################################
 
--- uda_ml/98-code/ClassifyNB.py
+++ uda_ml/98-code/ClassifyNB.py
@@ -1,3 +1,5 @@
+from sklearn.naive_bayes import GaussianNB
+
 def classify(features_train, labels_train):   
     ### import the sklearn module for GaussianNB
     ### create classifier
@@ -6,5 +8,8 @@ def classify(features_train, labels_train):
     
     
     ### your code goes here!
-    
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+
+    return clf
     
